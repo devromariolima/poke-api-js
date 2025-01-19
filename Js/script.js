d1b022256461fb57bde74380afe4150414196242
Romário Lima <devromariolima@gmail.com>
2025-01-19 19:38:24 -0400

    refactory: renderPokemon

--- Js/script.js
+++ Js/script.js
@@ -2,28 +2,31 @@ const pokemonId = document.querySelector('.pokemon_number');
 const pokemonName = document.querySelector('.pokemon_name');
 const pokemonImage = document.querySelector('.pokemon_image');
 const form = document.querySelector('.form');
-
 const input = document.querySelector('.input_search');
 
 const fetchPokemon = async (Pokemon) => {
 
-    const ApiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${Pokemon}`);
-    const data = await ApiResponse.json();
-    return data;
-
-
+    const ApiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${Pokemon.toLowerCase()}`);
+    if (ApiResponse.status == 200) {
+        const data = await ApiResponse.json();
+        return data;
+    }
 }
 
 const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
-    pokemonName.innerHTML = data.name;
-    pokemonId.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    if (data) {
+        pokemonName.innerHTML = data.name;
+        pokemonId.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        input.value = '';
+    } else {
+        pokemonName.innerHTML = 'Não encontrado!'
+        pokemonId.innerHTML = '';
+    }
 }
 
 form.addEventListener('submit', (event) => {
-
     event.preventDefault();
-    renderPokemon(input.value.toLowerCase());
-    input.value = '';
+    renderPokemon(input.value);
 });
